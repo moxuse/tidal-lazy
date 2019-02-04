5d2c74f73109952df5f90968d40458e26abc5cd8
moxuse <moxuse@gmail.com>
2019-02-04 19:12:39 +0900

    lazy''

--- src/Ex.hs
+++ src/Ex.hs
@@ -7,7 +7,7 @@ import Sound.Tidal.Context
 
 -- pttern boost
 
-msvo time s = whenmod 4 3 (within (0.4, 0.8) (striate' 4 (1/8)))
+msvo time s = whenmod 4 3 (within (0.4, 0.8) (chop 2))
   $ every 2 (within (0.6,0.8) (density 0.75) . (0.25<~))
   $ every 4 (within (0.5,1.0) (slow 2))
   $ every 6 (within (0.2,0.4) (hurry 2))
@@ -17,15 +17,15 @@ msvo time s = whenmod 4 3 (within (0.4, 0.8) (striate' 4 (1/8)))
   $ every 2 (off time (mvSpeed "-0.25 -0.5 3 -2")) 
   $ s
 
-msvoA time s = every 9 (within (0.3, 0.4) (striate' 4 (1/8)))
-  $ every 4 (off time (|+| speed "-0.5 2 [0.4 -0.3] -2"))
+msvoA time s = every 9 (within (0.3, 0.4) (chop 3))
+  $ every 4 (off time (|> speed "-0.5 2 [0.4 -0.3] -2"))
   $ every 7 (within (0.7, 0.9) (chop 2))
   $ every 2 (within (0.3, 0.5) (slow 2))
   $ s
 
-msvoS time s = every 9 (within (0.3, 0.4) (striate' 4 (1/8)))
-  $ every 6 (off time (|+| speed "-0.5 2 [~ -0.3] -2"))
-  $ every 5 (within (0.2, 0.5) (|+| disv "-0.2 -0.1" "3 2"))
+msvoS time s = every 9 (within (0.3, 0.4) (chop 2))
+  $ every 6 (off time (|> speed "-0.5 2 [~ -0.3] -2"))
+  $ every 5 (within (0.2, 0.5) (|> disv "-0.2 -0.1" "3 2"))
   $ every 7 (within (0.7, 0.9) (chop 2))
   $ every 2 (within (0.3, 0.5) (slow 2))
   $ whenmod 5 3 (within (0.2, 0.4) (density 2))
@@ -33,29 +33,29 @@ msvoS time s = every 9 (within (0.3, 0.4) (striate' 4 (1/8)))
 
 -- effect function wrapper
 
-mSlowDiv n s = off n (|+| speed (slow (n / 2) $ "[2 0.75] 0.666 [2 [0.5 0.125]*3]*2 0.6666"))
+mSlowDiv n s = off n (|+ speed (slow (n / 2) $ "[2 0.75] 0.666 [2 [0.5 0.125]*3]*2 0.6666"))
   $ s
 
 mDefault = mGain
-  |+| shape "0.25"
-  |+| binshfR
-  |+| slowPanS 3
+  |+ shape "0.15"
+  |+ binshfR
+  |+ slowPanS 3
 
 mGrav = mGain
-  |+| shape "0.2"
-  |+| slowPanR 3
-  |+| slowSpeedR 4
+  |+ shape "0.2"
+  |+ slowPanR 3
+  |+ slowSpeedR 4
 
-msvoB time s = every 2 (|+| mfDel (slow 3 (range 1 0.01 rand)))
+msvoB time s = every 2 (|> mfDel' (slow 3 (range 1 0.01 rand)))
     $ every 4 (within (0.5, 0.75) (slow 3))
-    $ every 12 (within (0.4, 0.8) (# unit "s") . (|+| accelerate "<0.76 0 -0.16>") . (|+| loop "<0.125 0.75 0.5>"))
+    $ every 12 (within (0.4, 0.8) (|> unit "s") . (|> accelerate "<0.76 0 -0.16>") . (|> loop "<0.125 0.75 0.5>"))
     $ every 3 (off time (stut 3 0.05 6) . (zoom (0.25, 0.75)))
     $ s
 
-msvoC time s = every 4 (|+| mfDel (slow 2 (range 1 0.01 rand)))
-    $ every 4 (within (0.5, 0.75) (# speed "1 0.75 2 1 0.8"))
-    $ every 8 (within (0.25, 1.0) (# speed "<1 0.75 0.5 1.125>"))
-    $ every 12 (within (0.4, 0.8) (# unit "s") . (|+| accelerate "<0.76 0 -0.16>") . (|+| loop "<0.125 0.75 0.5>"))
+msvoC time s = every 4 (|+ mfDel' (slow 2 (range 1 0.01 rand)))
+    $ every 4 (within (0.5, 0.75) (|> speed "1 0.75 2 1 0.8"))
+    $ every 8 (within (0.25, 1.0) (|> speed "<1 0.75 0.5 1.125>"))
+    $ every 12 (within (0.4, 0.8) (|> unit "s") . (|> accelerate "<0.76 0 -0.16>") . (|> loop "<0.125 0.75 0.5>"))
     $ s
 
 
@@ -68,15 +68,15 @@ so2 s = spaceOut [1, 0.1, 2.33, 2, 0.5] $ s
 
 mStut pt sp s = sometimes (stut 4 1 1) 
   $ s
-  |+| up (foldr (+) pt ["[0 5 7]*2", "<0 3>", "<0 5 8>"])
-  |+| up (foldl (-) "<0 1 0 3>" ["{0 0 2}/2", "<0 1>", "0 1", "0 3"])
-  |*| speed sp
+  |+ up (foldr (+) pt ["[0 5 7]*2", "<0 3>", "<0 5 8>"])
+  |+ up (foldl (-) "<0 1 0 3>" ["{0 0 2}/2", "<0 1>", "0 1", "0 3"])
+  |* speed sp
 
 mStut' pt sp s = sometimes (stut 4 1 1) 
   $ s
-  |+| n (foldr (+) pt ["[0 5 7]*2", "<0 3>", "<0 5 8>"])
-  |+| n (foldl (-) "<0 1 0 3>" ["{0 0 2}/2", "<0 1>", "0 1", "0 3"])
-  |*| speed sp
+  |+ n (foldr (+) pt ["[0 5 7]*2", "<0 3>", "<0 5 8>"])
+  |+ n (foldl (-) "<0 1 0 3>" ["{0 0 2}/2", "<0 1>", "0 1", "0 3"])
+  |* speed sp
 
 mvSpeed pt s = within (0.2, 0.85) (# speed (sometimesBy 0.75 ((slow "<0.75 2 1.25>") . (sometimes rev)) pt)) $ s
 
--- src/ExLazy.hs
+++ src/ExLazy.hs
@@ -16,33 +16,32 @@ binPats n = [
   (slow n $ "1/2 0 1/4 0 [1 0] 0/4 1 0*2 0/6 1 1/3 1 1/4 0 1/2 0/4 1"),
   (slow n $ "0 0 1/4 0/2 0 1/2 1 0/3 1/2 0 1 1/4 0/3 1? 0 1/5"),
   (slow n $ "0 1/4 1 0 1/4 0 0 1/4 [0 0/3] 0 1/6 0*2 1/2 0*2 1 0/6"),
-  (slow "<4 5 4 1.75>" "[0 [1 0 [1 0]]]/4 1/2 [0 1/2]/2 0 [1 0? 1]*2 1/3 [[0 1]/3 0] 0 1 0/2 1 [1 0? 0]/2 0 1 1"),
-  (slow "<5 4 6 2>" "0 1*2 [1/2 [0? 1]] 1 1/2 [1 [0 [1 0]?]?]/2 1? [0 [1 0 1]?]/3 1 1 0/2 [1 1 0] 1 0 0 1")]
+  (slow "<4 5 4 1.75>" "[0 [1 0 [1 0]]]/4 1/2 [0 1/2]/2 0 [1 0? 1]*2 1/3"),
+  (slow "<5 4 6>" "0 1*2 [1/2 [0? 1]] 1 1/2 [1 [0 [1 0]?]?]/2 1? [0 [1 0 1]?]/3")]
   
 
 seqlist = [
-  "<0 3 0 3 3 0 0 3 1>",
-  "<1 0 1 0 1 1 2 2 0 0 1 1>",
-  "<0 3 0 0 1 3 3 3>",
-  "<0 0 1 1 0 1 0 0 1 3 0>",  
-  "<0 0 1 0 1 1>",
-  "<1 1 3 1 3 2 1 3>",
-  "<0 0 3 3 1 0 0 1 1 3 3 2 2>"]
+  "0 3 0 3 3 0 0 3",
+  "1 0 1 0 1 1 2 2",
+  "0 3 0 0 1 3 3 3",
+  "0 2 1 2 0 1 0 0",  
+  "0 2 1 0 1 1 0 2",
+  "1 1 3 1 3 2 1 3",
+  "0 0 3 3 1 0 0 1"]
 
 pat0 bpt c = whenmod 3 2 (within (0.2, 0.8) (slow "2 0.75"))
   $ stack [
   ((0.125 ~>) $ gain bpt |+| s (hh' c))
   ,(gain (inverse <$> bpt) |+| s (sn' c))
   ,((0.125 <~) $ gain (inverse <$> bpt) |+| s (bd' c))]
-  # gain "[1 0.8 1]*2"
-
+  |+| gain "[1 0.8 1]*2"
 pat1 bpt c = whenmod 3 1 (within (0.5, 0.8) (slow 2)) 
   $ stack [
   ((0.125 ~>) $ gain bpt |+| s (hh' c))
   ,(gain bpt |+| s (slow 2 (cp' c)))
   ,(gain (inverse <$> bpt) |+| sound (sn' c))
   ,((0.125 <~) $ gain (inverse <$> bpt) |+| s (bd' c))]
-  # gain "[1 0.8 1 0.75]*2"
+  |+| gain "[1 0.8 1 0.75]*2"
 
 pat2 bpt c = whenmod 3 2 (within (0.2, 0.8) (slow "2 0.75"))
   $ stack[
@@ -51,22 +50,22 @@ pat2 bpt c = whenmod 3 2 (within (0.2, 0.8) (slow "2 0.75"))
   ,((0.125 <~) $ gain bpt |+| sound (hh' c))
   ,(gain (inverse <$> bpt) |+| sound (sn' c))
   ,(slow 4 $ (0.125 ~>) $ degradeBy 0.5 $ gain bpt |+| sound (slow 2 (ps' c)))]
-  # gain "[1 0.8 0.75]/2"
+  |+| gain "[1 0.8 0.75]/2"
 
 pat3 bpt c = stack [
   (gain bpt |+| s (hh' c))
   ,(gain (inverse <$> bpt) |+| s (sn' c))
   ,((0.125 <~) $ gain (inverse <$> bpt) |+| s (bd' c))]
-  # gain "[1 0.8 0.75]*2"
+  |+| gain "[1 0.8 0.75]*2"
 
 pat4 = whenmod 4 2 (off 0.175 (|+| speed "1.5"))
   $ whenmod 3 2 (within (0.2, 0.8) (slow "2 0.75"))
   $ sound "[mb2/4, mbd2 mhh2*2]/2 [[msn2*2 mcp]/2 ~ [mhh2*2 mbd2]/3 msn2]/4 ~ [msn2 mps]" # gain "1.1"
 pat5 = sound "{mpd2*2? ~ , [~  mbd2*2]*2 mhh2}" # gain "1.1"
 pat6 = whenmod 3 2 (within (0.25, 0.8) (slow "0.75 2"))
-  $ sound "{mbd2(2,5), mhh2:3*8, [mbd2 msn2*2 mnz]/3 [msn2*2 mb2]/2 , ~ mps ~ mbd2*2 mps}" |+| cut (choose[3, 2, 1]) # gain "1"
+  $ sound "{mbd2(2,5), mhh2:3*2, [mbd2 msn2*2 mnz]/3 [msn2*2 mb2]/2 , ~ mps ~ mbd2*2 mps}" |+| cut (choose[3, 2, 1]) # gain "1"
 pat7 = whenmod 4 2 (within (0.4, 0.8) (0.125<~))
-  $ sound "[mps/2 mhh2*2 mnt2/4 ~, [mhh2*3 msn2*2]/2, mhh2*12, mbd2(3,5) ~ mhh2*2]/3" # gain "1"
+  $ sound "[mps/2 mhh2*2 mnt2/4 ~, [mhh2*3 msn2*2]/2, mhh2*2, mbd2(3,5) ~ mhh2*2]/3" # gain "1"
 pat8 = sound "[mbd2(3,5), mhh2 msn2, mps/2 ml2/4 ~ mps/4]" # gain "1" 
 
 paatternList1 bpt c = [
@@ -92,7 +91,7 @@ paatternList2' bpt c = [
 chooseUr :: [(String, Pattern a)] -> IO (Pattern a)
 chooseUr pat = do
   p <- (randpick seqlist :: IO (Pattern String))
-  let m = ur 2 p pat []
+  let m = ur 8 p pat []
   return $ m
 
 chooseUr0 bpt = do
@@ -105,7 +104,7 @@ chooseUr0 bpt = do
 chooseUr1 c bpt = do
   p0 <- (randpick (paatternList1 bpt c))
   p1 <- (randpick (paatternList1 bpt c))
-  p2 <- (randpick (paatternList2' bpt c))
+  p2 <- (randpick (paatternList1 bpt c))
   p3 <- (randpick (paatternList2' bpt c))
   return [("0", p0), ("1", p1), ("2", p2), ("3", p3)]
 
@@ -122,6 +121,39 @@ chooseBpt n = (randpick (binPats n))
 lazy = (chooseBpt 2) >>= chooseUr0 >>= chooseUr
 
 lazy' :: Pattern Time -> DefaultInstrument -> IO (ControlPattern)
-
 lazy' n c = chooseBpt n >>= chooseUr1 c >>= chooseUr
+
+
+replicateN :: Int -> Pattern a -> Pattern a 
+replicateN n p =  (fastCat) (replicate n p)
+
+pickSeedA def = randpick [
+  fastAppend (euclid 3 13 (hh' def)) "~"
+  ,overlay (euclid 5 9 (bd' def)) (replicateN 2 (sn' def))
+  ,fastAppend (fastAppend (euclid 3 8 (bd' def)) "~") (replicateN 3(cp' def))
+  ]
+
+pickSeedB def = randpick [
+  overlay (replicateN 6 (sn' def)) $ fastAppend (euclid 5 13 (bd' def)) "~"
+  ,fastAppend "~" (fastAppend (replicateN 5 (sn' def)) "~")
+  ,overlay (replicateN 4 (bd' def)) (fastAppend (euclid 3 6 (cp' def)) "~")
+  ,(euclid 8 15 (bd' def))
+  ,fastAppend "~" (euclid 8 15 (bd' def))
+  ]
+
+partialPat pat instr = do 
+  cp <- pickSeedA instr
+  dp <- pickSeedB instr
+  return (sound (sew pat cp dp))
+
+lazy'' :: Pattern Bool -> DefaultInstrument -> IO (ControlPattern)
+lazy'' pat instr = do
+  pa <- partialPat pat instr
+  pb <- partialPat pat instr
+  pc <- partialPat pat instr
+  return $ stack [
+      pa
+    , pb
+    , pc
+    ] 
 
